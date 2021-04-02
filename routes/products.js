7b7afb5371813ab7e96073446db6bf9ed8eb0089
mollyexten <molly.exten@gmail.com>
2021-04-02 15:18:26 -0400

    created seed

--- routes/products.js
+++ routes/products.js
@@ -1,4 +1,15 @@
+// Import mysterious Router
 const { Router } = require("express")
-// const controllers = require("../controllers/product")
+const controllers = require("../controllers/products")
+
+// Instantiate the module
 const router = Router()
+
+// Define the routes
+router.get("/", controllers.getProducts)
+router.get("/:id", controllers.getProduct)
+router.post("/", controllers.createProduct)
+router.put("/:id", controllers.updateProduct)
+router.delete("/:id", controllers.deleteProduct)
+
 module.exports = router
