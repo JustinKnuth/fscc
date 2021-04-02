2797c58c9692e9a5c3cf0f0630903a3a4d01b18f
mollyexten <molly.exten@gmail.com>
2021-04-02 16:27:26 -0400

    added restrict function and imported it in product route for post, put, and delete

--- routes/products.js
+++ routes/products.js
@@ -1,6 +1,7 @@
 // Import mysterious Router
 const { Router } = require("express")
 const controllers = require("../controllers/products")
+const restrict = require('../helpers/restrict')
 
 // Instantiate the module
 const router = Router()
@@ -8,8 +9,8 @@ const router = Router()
 // Define the routes
 router.get("/", controllers.getProducts)
 router.get("/:id", controllers.getProduct)
-router.post("/", controllers.createProduct)
-router.put("/:id", controllers.updateProduct)
-router.delete("/:id", controllers.deleteProduct)
+router.post("/", restrict, controllers.createProduct)
+router.put("/:id", restrict, controllers.updateProduct)
+router.delete("/:id", restrict, controllers.deleteProduct)
 
 module.exports = router
