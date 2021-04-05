af6c7989bd97fc5e30e254d3e78b96f5ff3c7a57
mollyexten <molly.exten@gmail.com>
2021-04-05 15:41:45 -0400

    added layout component to shared and imported into products component

--- client/src/screens/Products/Products.jsx
+++ client/src/screens/Products/Products.jsx
@@ -1,39 +1,45 @@
-import { useEffect, useState } from "react"
-import "./Products.css"
-import Product from "../../components/Product/Product"
-import { getProducts } from "../../services/products"
-
+import { useEffect, useState } from "react";
+import "./Products.css";
+import Product from "../../components/Product/Product";
+import { getProducts } from "../../services/products";
+import Layout from "../../components/shared/Layout/Layout";
 
 const Products = () => {
-  const [allProducts, setAllProducts] = useState([])
+  const [allProducts, setAllProducts] = useState([]);
 
   useEffect(() => {
     const fetchProducts = async () => {
-      const products = await getProducts()
-      console.log(products)
-      setAllProducts(products)
-    }
-    fetchProducts()
-  }, [])
-
+      const products = await getProducts();
+      console.log(products);
+      setAllProducts(products);
+    };
+    fetchProducts();
+  }, []);
 
   if (!allProducts) {
-    return
-    
+    return;
+    <Layout>
       <h3>Hello</h3>
-    
+    </Layout>;
   }
 
-  const productsJSX = allProducts.map((product, index) =>
-    <Product _id={product._id} name={product.name} imgURL={product.imgURL} key={index} />)
+  const productsJSX = allProducts.map((product, index) => (
+    <Product
+      _id={product._id}
+      name={product.name}
+      imgURL={product.imgURL}
+      key={index}
+    />
+  ));
 
   return (
-    <div className="products">
-      <h3>Products</h3>
-      <div>{productsJSX}</div>
-    </div>
-  )
-}
+    <Layout>
+      <div className="products">
+        <h3>Products</h3>
+        <div>{productsJSX}</div>
+      </div>
+    </Layout>
+  );
+};
 
-
-export default Products
+export default Products;
