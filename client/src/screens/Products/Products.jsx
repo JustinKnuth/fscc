2542533fd844138ad96c68ed31a70ca9e11f676d
Dylan Ross <dylanross@Dylans-MacBook-Pro.local>
2021-04-05 13:00:15 -0400

    product screen

--- client/src/screens/Products/Products.jsx
+++ client/src/screens/Products/Products.jsx
@@ -1,9 +1,39 @@
-import React, { useEffect, useState } from "react"
+import { useEffect, useState } from "react"
 import "./Products.css"
-import Product from "../../components/Product"
+import Product from "../../components/Product/Product"
 import { getProducts } from "../../services/products"
 
 
-const Products = (props) => {
-  const [allProducts, ]
+const Products = () => {
+  const [allProducts, setAllProducts] = useState([])
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const products = await getProducts()
+      console.log(products)
+      setAllProducts(products)
+    }
+    fetchProducts()
+  }, [])
+
+
+  if (!allProducts) {
+    return
+    
+      <h3>Hello</h3>
+    
+  }
+
+  const productsJSX = allProducts.map((product, index) =>
+    <Product _id={product._id} name={product.name} imgURL={product.imgURL} key={index} />)
+
+  return (
+    <div className="products">
+      <h3>Products</h3>
+      <div>{productsJSX}</div>
+    </div>
+  )
 }
+
+
+export default Products
