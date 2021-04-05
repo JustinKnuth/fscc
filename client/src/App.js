b35166fac00cbbb0c1e38129d3b47776fa0061eb
Justin Kay <justinkay@Justins-MacBook-Pro.local>
2021-04-05 13:40:18 -0400

    working with nav

--- client/src/App.js
+++ client/src/App.js
@@ -3,24 +3,25 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import './App.css'
 import { verifyUser } from './services/users';
 import Products from "./screens/Products/Products"
-
+import Nav from "./components/shared/Nav/Nav"
 
 function App() {
-  // const [user, setUser] = useState(null)
+  const [user, setUser] = useState(null)
   const [toggleFetch, setToggleFetch] = useState(false)
   
-  // useEffect(() => {
-  //   const fetchUser = async () => {
-  //     const user = await verifyUser()
-  //     user ? setUser(user) : setUser(null)
-  //   }
-  //   fetchUser()
-  // }, [])
+  useEffect(() => {
+    const fetchUser = async () => {
+      const user = await verifyUser()
+      user ? setUser(user) : setUser(null)
+    }
+    fetchUser()
+  }, [])
 
-  // const clearUser = () => setUser(null)
+  const clearUser = () => setUser(null)
 
   return (
     <div className="App">
+      <Nav />
       <Switch>
         <Route exact path="/">
         </Route>
