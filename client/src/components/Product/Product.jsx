2c488780fd0d692849cd3458b4fbe86a7ac3ad30
Dylan Ross <dylanross@Dylans-MacBook-Pro.local>
2021-04-05 11:33:54 -0400

    created Product component

---
+++ client/src/components/Product/Product.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Product.css"
+import { Link } from "react-router-dom"
+
+const Product = (props) => {
+  return (
+    <>
+      <Link className="product" to={`/products/${props.id}`}>
+        <img className="product-image" src={props.imgURL} alt={props.name} />
+        <h3 className="product-name">{props.name}</h3>
+  </Link>
+  </>
+  )
+}
+
+export default Product
