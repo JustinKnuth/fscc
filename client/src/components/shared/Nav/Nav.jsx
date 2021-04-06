e08da5804e323d013984b913559b469a9d1d8c31
Justin Kay <justinkay@Justins-MacBook-Pro.local>
2021-04-06 17:53:28 -0400

    hamnburger menu

--- client/src/components/shared/Nav/Nav.jsx
+++ client/src/components/shared/Nav/Nav.jsx
@@ -1,5 +1,6 @@
 import "./Nav.css";
 import { NavLink } from "react-router-dom";
+import { useState, useEffect } from "react"
 
 const authenticatedOptions = (
   <>
@@ -32,25 +33,78 @@ const alwaysOptions = (
 );
 
 const Nav = ({ user }) => {
+  const [hamburger, setHamburger] = useState(true);
+  const [visible, setVisible] = useState(true);
+  useEffect(() => {
+    const handleResize = (e) => {
+      if (window.innerWidth > 425) {
+        setHamburger(true);
+        setVisible(true);
+      } else if (window.innerWidth <= 425) {
+        setHamburger(false);
+      }
+    }
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    }
+  }, [])
   return (
+
     <nav>
+      
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/1200px-Hamburger_icon.svg.png"
+          className="hamburger"
+          alt="hamburger"
+          onClick={() => setHamburger(!hamburger)}
+        />
+    
       <img className="logo" src="/fscclogolight.png" alt="logo" />
+
       <div className="nav">
         <NavLink to="/">
           <h1 className="nav-header">full stack coffee co.</h1>
         </NavLink>
         <div className="links">
           {/* If there is a user, render Welcome <user> */}
-          {user && <div className="link welcome">Welcome, {user.username}</div>}
+          {user && <div className="user-welcome">Welcome, {user.username}</div>}
           {/* Always render this */}
           {alwaysOptions}
+
           {/* If there is user, render authenticatedOptions, 
           otherwise render unauthenticatedOptions */}
           {user ? authenticatedOptions : unauthenticatedOptions}
         </div>
       </div>
+
     </nav>
+
+
+
+
+
+
+
+
+
+
   );
 };
 
 export default Nav;
+
+
+  // return (
+  //   <nav>
+  //     
+  //     <div className="nav-items" style={{ display: hamburger && visible ? `flex` : `none` }}>
+  //       <Link to="/">
+  //         Home
+  //       </Link>
+  //       <Link to="/new">
+  //         New Sitcom
+  //       </Link>
+  //     </div>
+  //   </nav>
+  // );
