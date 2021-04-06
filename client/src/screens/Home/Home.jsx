c6be508b4600d129087f9dfb9cb211e09da71771
paulmoschetti <paulmoschetti93@gmail.com>
2021-04-06 10:06:54 -0600

    basic carousel

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import Layout from '../../components/shared/Layout/Layout';
+import Carousel from 'nuka-carousel';
 import './Home.css'
 
 export default function Home() {
@@ -8,6 +9,15 @@ export default function Home() {
     <div className="bg-image">
       <Layout>
         <div className="brand">
+          <div  className="carousel">
+          <Carousel>
+            <img className="imgz" src="https://images.unsplash.com/photo-1612668196612-70262cad2ad7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80" />
+            <img className="imgz" src="https://images.unsplash.com/photo-1475296204602-08d15839e95f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" />
+            <img className="imgz" src="https://images.unsplash.com/photo-1472495010058-65576a9959e4?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1500&q=80" />
+            <img className="imgz" src="https://images.unsplash.com/photo-1570936449947-d0a30aedfff3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80" />
+            <img className="imgz" src="https://images.unsplash.com/photo-1441757801704-6a71cffed732?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NjR8fGNvZmZlZXxlbnwwfDB8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"/>
+          </Carousel>
+          </div>
           <Link>
             <div id="shop">
         <h2 id='shop'>Shop</h2>
