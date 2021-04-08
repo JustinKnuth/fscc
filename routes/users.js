1e490acebd40ab4d5f026dfd4b8594ab3e7dc55c
paulmoschetti <paulmoschetti93@gmail.com>
2021-04-08 11:01:26 -0600

    start of cart

--- routes/users.js
+++ routes/users.js
@@ -7,5 +7,6 @@ router.post('/sign-up', controllers.signUp)
 router.post('/sign-in', controllers.signIn)
 router.get('/verify', controllers.verify)
 router.post('/change-password', controllers.changePassword)
+router.put('/users/:id/add-to-cart/:productId', controllers.addToCart)
 
 module.exports = router
