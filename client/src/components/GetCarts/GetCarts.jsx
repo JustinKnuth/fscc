94e4a468fa2a76cdad603a60b3eaedc9b73a3a9b
Dylan Ross <dylanross@Dylans-MacBook-Pro.local>
2021-04-08 17:54:39 -0400

    deleted Brew screen

--- client/src/components/GetCarts/GetCarts.jsx
+++ client/src/components/GetCarts/GetCarts.jsx
@@ -5,7 +5,7 @@ const GetCarts = (props) => {
   console.log(props);
   return (
     <>
-      <img className="cart-item-image" src={props.imgURL} alt={props.name} />
+      {/* <img className="cart-item-image" src={props.imgURL} alt={props.name} /> */}
       <h2 className="cart-item-name">{props.name}</h2>
       <h2 className="cart-item-price">{props.price}</h2>
     </>
--- client/src/screens/Brew/Brew.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Brew() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
