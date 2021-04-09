54d7fac6f86b91af429b196b78255216ad717092
Justin Kay <justinkay@Justins-MacBook-Pro.local>
2021-04-09 11:29:07 -0400

    cart page

--- client/src/screens/Cart/Cart.jsx
+++ client/src/screens/Cart/Cart.jsx
@@ -31,7 +31,7 @@ const Cart = (props) => {
   const cartItemsJSX = cartItems.map((cartItem, index) => (
     <GetCarts
       _id={cartItem._id}
-      // image={cartItem.imgURL}
+      image={cartItem.imgURL}
       name={cartItem.name}
       price={cartItem.price}
       key={index}
@@ -41,10 +41,24 @@ const Cart = (props) => {
 console.log(cartItems)
   return (
     <Layout user={props.user}>
-      <h3>Cart</h3>
-      <div className="cart-items-container">
-        {cartItemsJSX}
-      </div>
+      <h3>cart</h3>
+     
+      <table className="cart-table">
+        <tr>
+          <td>
+            
+          </td>
+          <td>
+          <h2 className="cart-table-header">item</h2>
+          </td>
+          <td>
+          <h2 className="cart-table-header">price</h2>
+          </td>
+        </tr>
+      {cartItemsJSX}
+        </table>
+        
+
     </Layout>
   )
 }
